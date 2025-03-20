4b9c61d8000389e2aecf3037caba9cbc88287d65
Vitor Oliveira <vitorroliveiraa@outlook.com.br>
2025-03-20 13:44:03 -0300

    Update server to listen on all interfaces

--- src/server.ts
+++ src/server.ts
@@ -2,6 +2,6 @@ import "express-async-errors";
 import app from "./app.ts";
 import "dotenv/config";
 
-app.listen(process.env.API_PORT, () => {
-    console.log("🚀 App is running at http://localhost:" + process.env.API_PORT);
+app.listen(Number(process.env.API_PORT!), "0.0.0.0", () => {
+  console.log("🚀 App is running at http://localhost:" + process.env.API_PORT);
 });
