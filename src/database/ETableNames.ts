3a5a9e07961080988fddcd98184c9faae080c280
Vitor Oliveira <vitorroliveiraa@outlook.com.br>
2025-04-16 11:00:34 -0300

    feat: add new tables

--- src/database/ETableNames.ts
+++ src/database/ETableNames.ts
@@ -1,5 +1,7 @@
 export enum ETableNames {
   users = "users",
   wards = "wards",
+  speakers = "speakers",
+  church_members = "church_members",
   passwordResetTokens = "password_reset_tokens",
 }
