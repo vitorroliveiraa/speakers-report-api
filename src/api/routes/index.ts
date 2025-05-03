eac866d642b45ef88b861cee99641e3207f28f83
Vitor Oliveira <vitorroliveiraa@outlook.com.br>
2025-05-03 12:47:40 -0300

    feat: update API routes to remove versioning from paths

--- src/api/routes/index.ts
+++ src/api/routes/index.ts
@@ -5,8 +5,8 @@ import { speakersRoutes } from "./speakersRoutes.ts";
 
 const router = Router();
 
-router.use("/api/v1/users", usersRoutes);
-router.use("/api/v1/auth", authRoutes);
-router.use("/api/v1/speakers", speakersRoutes);
+router.use("/users", usersRoutes);
+router.use("/auth", authRoutes);
+router.use("/speakers", speakersRoutes);
 
 export { router };
