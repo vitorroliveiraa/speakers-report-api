b10426057a8996fc26ecffcca4fa30c72bd741fa
Vitor Oliveira <vitorroliveiraa@outlook.com.br>
2025-05-07 18:54:45 -0300

    refactor: enhance knex configuration and secret management

--- src/database/knexfile.ts
+++ src/database/knexfile.ts
@@ -6,51 +6,86 @@ interface KnexConfig {
   [key: string]: Knex.Config;
 }
 
+const baseConfig: Partial<Knex.Config> = {
+  client: "pg",
+  migrations: {
+    tableName: "knex_migrations",
+    extension: process.env.NODE_ENV === "development" ? "ts" : "js",
+    directory: path.join(
+      process.cwd(),
+      process.env.NODE_ENV === "development" ? "migrations" : "migrations"
+    ),
+  },
+  seeds: {
+    directory: path.join(
+      process.cwd(),
+      process.env.NODE_ENV === "development" ? "seeds" : "seeds"
+    ),
+    extension: process.env.NODE_ENV === "development" ? "ts" : "js",
+    timestampFilenamePrefix: true,
+  },
+};
+
+const readSecret = (path: string): string => {
+  console.log("🚩 path", path);
+  if (process.env.NODE_ENV !== "production") {
+    return "";
+  }
+
+  try {
+    return require("fs").readFileSync(path, "utf8").trim();
+  } catch (e) {
+    const envVarName = path.split("/").pop()?.toUpperCase() || "";
+    return process.env[envVarName] || "";
+  }
+};
+
+const connection =
+  process.env.NODE_ENV === "production"
+    ? {
+        host: process.env.DB_HOST,
+        port: parseInt(process.env.DB_PORT || "5432"),
+        user: readSecret(process.env.DB_PROD_USER_FILE!),
+        password: readSecret(process.env.DB_PROD_PWD_FILE!),
+        database: process.env.DB_NAME,
+        ssl:
+          process.env.DB_SSL === "true" ? { rejectUnauthorized: false } : false,
+      }
+    : {
+        host: "localhost",
+        port: 5432,
+        user: "admin",
+        password: "admin",
+        database: "speakers-report-dev",
+        ssl: false,
+      };
+
 const knexConfig: KnexConfig = {
   development: {
-    client: "pg",
-    connection: {
-      host: process.env.DB_HOST || "localhost",
-      port: parseInt(process.env.DB_PORT || "5432"),
-      user: process.env.DB_USER || "admin",
-      password: process.env.DB_PWD || "admin",
-      database: process.env.DB_NAME || "speakers-report",
-    },
+    ...baseConfig,
+    connection,
     pool: {
-      min: 2, // Mínimo de conexões no pool
-      max: 10, // Máximo de conexões no pool
-      acquireTimeoutMillis: 30000, // Tempo limite para adquirir uma conexão (em milissegundos)
-      idleTimeoutMillis: 60000, // Tempo limite de inatividade para uma conexão (em milissegundos)
-      reapIntervalMillis: 1000, // Intervalo para tentativas de reconexão (em milissegundos)
-    },
-    migrations: {
-      tableName: "knex_migrations",
-      extension: "ts",
-      directory: path.join(process.cwd(), "migrations"),
-    },
-    seeds: {
-      directory: path.join(process.cwd(), "seeds"),
-      extension: "ts",
-      timestampFilenamePrefix: true,
+      min: 2,
+      max: 10,
+      acquireTimeoutMillis: 30000,
+      idleTimeoutMillis: 60000,
+      reapIntervalMillis: 1000,
+      createRetryIntervalMillis: 200,
+      createTimeoutMillis: 30000,
     },
   },
   production: {
-    client: "pg",
-    connection: {
-      connectionString: process.env.CONNECTION_STRING,
-      ssl: {
-        rejectUnauthorized: false,
-      },
-    },
-    migrations: {
-      tableName: "knex_migrations",
-      extension: "ts",
-      directory: path.join(process.cwd(), "migrations"),
-    },
-    seeds: {
-      directory: path.join(process.cwd(), "seeds"),
-      extension: "ts",
-      timestampFilenamePrefix: true,
+    ...baseConfig,
+    connection,
+    pool: {
+      min: 2,
+      max: 10,
+      acquireTimeoutMillis: 60000,
+      idleTimeoutMillis: 30000, // Tempo máximo que uma conexão pode ficar inativa
+      createRetryIntervalMillis: 1000, // Intervalo entre as tentativas de criar uma nova conexão
+      createTimeoutMillis: 60000, // Tempo máximo para criar uma nova conexão
+      propagateCreateError: false, // Não propaga erros de conexão para evitar quebrar a aplicação
+      reapIntervalMillis: 1000, // Verifica conexões com mais frequência
     },
   },
 };
