056ae45d22b7f4e91271954fc1c4cce12b7b147a
Vitor Oliveira <vitorroliveiraa@outlook.com.br>
2025-05-23 14:35:42 -0300

    refactor: remove commented code and improve clarity in database index file

--- src/database/index.ts
+++ src/database/index.ts
@@ -2,13 +2,10 @@ import knex from "knex";
 import config from "./knexfile.ts";
 import "dotenv/config";
 
-// Definir o ambiente atual, sendo 'development' por padrão
 const environment = process.env.NODE_ENV || "development";
-// console.log("environment", environment);
 
 const knexConfig = config[environment];
 
-// Criar a instância do Knex com a configuração adequada
 const db = knex(knexConfig);
 
 export default db;
