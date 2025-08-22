4bc0de6d6692225896d15031148ca0c23165ecf5
Vitor Oliveira <vitorroliveiraa@outlook.com.br>
2025-08-22 16:35:58 -0300

    feat: update api_host

--- entrypoint-nginx.sh
+++ entrypoint-nginx.sh
@@ -1,23 +1,17 @@
 #!/bin/sh
-# Garante que o script pare se algum comando falhar
 set -e
 
-# O nome do serviço da API e a porta, conforme definido no docker-swarm.yml
-API_HOST="api-production"
-API_PORT="3000"
+HOST="speakers-report_api-production"
+PORT="3000"
 
-# Loop 'until' que tenta se conectar ao host da API na porta especificada.
-# O comando 'nc -z' (netcat) verifica se a porta está aberta sem enviar dados.
-echo "Aguardando a API em ${API_HOST}:${API_PORT}..."
-until nc -z "${API_HOST} ${API_PORT}"; do
+echo "Verificando se a API em ${HOST}:${PORT} está disponível..."
+
+while ! nc -z ${HOST} ${PORT}; do
   echo "API indisponível. Tentando novamente em 5 segundos..."
   sleep 5
 done
 
-echo "API está disponível! Iniciando o Nginx..."
+echo "API disponível! Iniciando o Nginx..."
 
-# O comando 'exec "$@"' é muito importante.
-# Ele substitui o processo do shell pelo comando original do container (o Nginx).
-# Isso garante que o Nginx se torne o processo principal (PID 1),
-# permitindo que ele receba sinais do Docker corretamente (como para parar o container).
-exec "$@"
+# Execute o comando padrão do Nginx
+exec nginx -g "daemon off;"
